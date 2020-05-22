1d0cc9f6c4b9f34391c5d63d88659b26d465e1b9
daviddecraene <david.ma.decraene@gmail.com>
2020-05-22 08:10:17 +0200

    adding axis

---
+++ Input/KeyAxis.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Wombat
+{
+    public class KeyAxis<T> where T : System.Enum
+    {
+        public readonly KeyConfig<T> negative;
+        public readonly KeyConfig<T> positive;
+
+        public KeyAxis(KeyConfig<T> negative, KeyConfig<T> positive)
+        {
+            this.negative = negative;
+            this.positive = positive;
+        }
+
+        public float GetValue()
+        {
+            if (negative.IsHeldDown())
+            {
+                return -1;
+
+            }
+            else if (positive.IsHeldDown())
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
