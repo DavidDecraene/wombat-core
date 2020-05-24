894c07b0f499e27dca585fc131aa6b7e41c00637
daviddecraene <david.ma.decraene@gmail.com>
2020-05-24 09:04:54 +0200

    cursor

---
+++ UI/MouseCursor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Wombat
+{
+    [RequireComponent(typeof(RectTransform))]
+    public class MouseCursor : MonoBehaviour
+    {
+
+        public bool locked = true;
+        public Vector3 offset = new Vector3();
+        public RectTransform RectTransform { get; private set; }
+
+        void Start()
+        {
+            Cursor.visible = false;
+            RectTransform = GetComponent<RectTransform>();
+            RectTransform.position = new Vector3(Screen.width / 2, Screen.height / 2) - offset;
+        }
+
+        void Update()
+        {
+            if (!locked)
+            {
+                RectTransform.position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, RectTransform.position.z);
+            }
+        }
+
+
+        public Ray ToRay(Camera bounds)
+        {
+            return bounds.ScreenPointToRay(transform.position);
+        }
+
+    }
+}
